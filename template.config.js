6c4f0892c2cede3d5d447fae4c58e8c2a4cd44bb
Notorious112 <55203625+Notorious112@users.noreply.github.com>
2021-09-01 13:35:57 +0300

    Update template.config.js
    Update template

--- template.config.js
+++ template.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   // Placeholder used to rename and replace in files
   // package.json, index.json, android/, ios/
-  placeholderName: "ProjectName",
+  placeholderName: "MyApp",
 
   // Directory with template
   templateDir: "./template",
