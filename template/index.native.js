cf34ec52998b85c1d8fdba3f5377e3e735bc2001
Notorious112 <55203625+Notorious112@users.noreply.github.com>
2021-09-01 15:24:51 +0300

    templateName in app.json

--- template/index.native.js
+++ template/index.native.js
@@ -4,6 +4,6 @@
 
 import { AppRegistry } from 'react-native';
 import App from './src/app/App';
-import { name as appName } from './src/app.json';
+import { name as appName } from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
--- template/src/index.js
+++ template/src/index.js
@@ -1,6 +1,6 @@
 import { AppRegistry } from 'react-native';
 import App from './app/App';
-import { name as appName } from './app.json';
+import { name as appName } from '../app.json';
 import './utils/icons';
 
 AppRegistry.registerComponent(appName, () => App);
