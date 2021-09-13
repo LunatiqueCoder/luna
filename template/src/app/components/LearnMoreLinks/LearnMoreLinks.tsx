5a8d42933bc10f369ac4963e8ebc0123435db976
Notorious112 <55203625+Notorious112@users.noreply.github.com>
2021-09-13 11:39:28 +0300

    updated types example

--- template/src/app/components/LearnMoreLinks/LearnMoreLinks.tsx
+++ template/src/app/components/LearnMoreLinks/LearnMoreLinks.tsx
@@ -1,57 +1,6 @@
-import {StyleSheet, Text, TouchableOpacity, View, Linking} from 'react-native';
 import React, {FC, Fragment} from 'react';
-
-const links = [
-  {
-    id: 1,
-    title: 'The Basics',
-    link: 'https://reactnative.dev/docs/tutorial',
-    description: 'Explains a Hello World for React Native.'
-  },
-  {
-    id: 2,
-    title: 'Style',
-    link: 'https://reactnative.dev/docs/style',
-    description: 'Covers how to use the prop named style which controls the visuals.'
-  },
-  {
-    id: 3,
-    title: 'Layout',
-    link: 'https://reactnative.dev/docs/flexbox',
-    description: 'React Native uses flexbox for layout, learn how it works.'
-  },
-  {
-    id: 4,
-    title: 'Components',
-    link: 'https://reactnative.dev/docs/components-and-apis',
-    description: 'The full list of components and APIs inside React Native.'
-  },
-  {
-    id: 5,
-    title: 'Navigation',
-    link: 'https://reactnative.dev/docs/navigation',
-    description: 'How to handle moving between screens inside your application.'
-  },
-  {
-    id: 6,
-    title: 'Networking',
-    link: 'https://reactnative.dev/docs/network',
-    description: 'How to use the Fetch API in React Native.'
-  },
-  {
-    id: 7,
-    title: 'Help',
-    link: 'https://reactnative.dev/help',
-    description: 'Need more help? There are many other React Native developers who may have the answer.'
-  },
-  {
-    id: 8,
-    title: 'Follow us on Twitter',
-    link: 'https://twitter.com/reactnative',
-    description:
-      'Stay in touch with the community, join in on Q&As and more by following React Native on Twitter.'
-  }
-];
+import {StyleSheet, Text, TouchableOpacity, View, Linking} from 'react-native';
+import {links} from 'static/constants';
 
 export const LearnMoreLinks: FC = () => {
   return (
---
+++ template/src/types/links.ts
@@ -0,0 +1,8 @@
+declare module 'learn-more-types' {
+  export interface ILink {
+    id: number;
+    title: string;
+    link: string;
+    description: string;
+  }
+}
