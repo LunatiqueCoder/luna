1dd352877df38d5ad1ce381137258471f8ec15e2
Notorious112 <55203625+Notorious112@users.noreply.github.com>
2021-09-13 12:24:59 +0300

    fix tests eslint

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    node: true
+    node: true,
+    jest: true
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended'],
   parser: '@typescript-eslint/parser',
