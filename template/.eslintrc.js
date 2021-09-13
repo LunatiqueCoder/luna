9b9146879476c4f55a4c50bf02279d9cfcdf4046
Notorious112 <55203625+Notorious112@users.noreply.github.com>
2021-09-13 12:34:01 +0300

    eslint config

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     }
   },
   rules: {
+    '@typescript-eslint/no-var-requires': 'off',
     indent: ['error', 2],
     'linebreak-style': ['error', 'unix'], // https://adaptivepatchwork.com/2012/03/01/mind-the-end-of-your-line/
     quotes: ['error', 'single'],
