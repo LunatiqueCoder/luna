e2a99ec04ada633376ac0656350642b10db78649
Notorious112 <cristee95@gmail.com>
2021-09-21 11:37:37 +0300

    IOS integration

--- template/src/app/App.tsx
+++ template/src/app/App.tsx
@@ -4,18 +4,21 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Home, Details} from 'app/pages';
+import {SafeAreaView} from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 const App: FC = () => {
   return (
     <SafeAreaProvider>
-      <NavigationContainer>
-        <Tab.Navigator initialRouteName='Home'>
-          <Tab.Screen component={Home} key={'Home'} name={'Home'} />
-          <Tab.Screen component={Details} key={'Details'} name={'Details'} />
-        </Tab.Navigator>
-      </NavigationContainer>
+      <SafeAreaView style={{flex: 1}}>
+        <NavigationContainer>
+          <Tab.Navigator initialRouteName='Home'>
+            <Tab.Screen component={Home} key={'Home'} name={'Home'} />
+            <Tab.Screen component={Details} key={'Details'} name={'Details'} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </SafeAreaView>
     </SafeAreaProvider>
   );
 };
