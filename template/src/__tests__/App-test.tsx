716f9deb3448f31d81d18817d996a6346ac9731c
Mike Hardy <github@mikehardy.net>
2021-10-28 09:54:15 -0500

    test(jest): fix jest test execution

--- template/src/__tests__/App-test.tsx
+++ template/src/__tests__/App-test.tsx
@@ -3,8 +3,10 @@
  */
 
 import 'react-native';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore -- the import is needed for tests
 import React from 'react';
-import App from '../app';
+import App from '../app/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
