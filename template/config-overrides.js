824f77c347a58b150b08dbfcda7452bf52ba03ec
Notorious112 <cristee95@gmail.com>
2021-10-31 19:08:11 +0200

    feature - remove react-native-elements

--- template/config-overrides.js
+++ template/config-overrides.js
@@ -20,9 +20,6 @@ module.exports = override(
   }),
   babelInclude([
     path.resolve(__dirname, 'src'),
-    path.resolve(__dirname, 'node_modules/react-native-elements'),
-    path.resolve(__dirname, 'node_modules/react-native-vector-icons'),
-    path.resolve(__dirname, 'node_modules/react-native-dynamic-vector-icons'),
-    path.resolve(__dirname, 'node_modules/react-native-ratings')
+    path.resolve(__dirname, 'node_modules/react-native-vector-icons')
   ])
 );
