ced4f9349e573401c5809cf98d120132b5bef0c9
Notorious112 <cristee95@gmail.com>
2021-11-08 22:27:56 +0200

    (fix) - remove SafeAreaView from App.tsx

--- template/src/app/App.tsx
+++ template/src/app/App.tsx
@@ -4,21 +4,18 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createMaterialTopTabNavigator} from '@react-navigation/material-top-tabs';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Home, Details} from 'app/pages';
-import {SafeAreaView} from 'react-native';
 
 const Tab = createMaterialTopTabNavigator();
 
 const App = (): JSX.Element => {
   return (
     <SafeAreaProvider>
-      <SafeAreaView style={{flex: 1}}>
-        <NavigationContainer>
-          <Tab.Navigator initialRouteName='Home'>
-            <Tab.Screen component={Home} key={'Home'} name={'Home'} />
-            <Tab.Screen component={Details} key={'Details'} name={'Details'} />
-          </Tab.Navigator>
-        </NavigationContainer>
-      </SafeAreaView>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='Home'>
+          <Tab.Screen component={Home} key={'Home'} name={'Home'} />
+          <Tab.Screen component={Details} key={'Details'} name={'Details'} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 };
