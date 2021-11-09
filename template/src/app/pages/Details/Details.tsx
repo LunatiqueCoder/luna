f4c0031fedbc6c9bbfadde2c9b646bb29843f063
Notorious112 <cristee95@gmail.com>
2021-11-09 16:29:55 +0200

    (feature) - bump versions and update SafeAreaView usage

--- template/src/app/pages/Details/Details.tsx
+++ template/src/app/pages/Details/Details.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Text, SafeAreaView, StyleSheet} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Details = (): JSX.Element => (
