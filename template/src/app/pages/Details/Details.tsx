412f3fa50c7d88d8a4b5511eeb9e436ff90be180
Notorious112 <cristee95@gmail.com>
2021-11-09 16:36:38 +0200

    (fix) SafeAreaView

--- template/src/app/pages/Details/Details.tsx
+++ template/src/app/pages/Details/Details.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import {Text, StyleSheet} from 'react-native';
-import {SafeAreaView} from 'react-native-safe-area-context';
+import {Text, StyleSheet, View} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 export const Details = (): JSX.Element => (
-  <SafeAreaView style={styles.background}>
+  <View style={styles.background}>
     <Icon name='rocket' size={30} color='#900' />
     <Text>If you see a rocket, everything is working!</Text>
-  </SafeAreaView>
+  </View>
 );
 
 const styles = StyleSheet.create({
