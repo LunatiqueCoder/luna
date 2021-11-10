3e45783d68dbb14c48ea8269321bd998f22d4cff
Notorious112 <cristee95@gmail.com>
2021-11-10 16:10:29 +0200

    (fix) Update Home instructions

--- template/src/app/navigation/pages/Home/Home.tsx
+++ template/src/app/navigation/pages/Home/Home.tsx
@@ -13,8 +13,8 @@ export const Home = (): JSX.Element => {
         <Header />
         <SafeAreaView>
           <Section title='Step One'>
-            Edit <Text style={styles.highlight}>src/app/pages/Home.tsx</Text> to change this screen and then
-            come back to see your edits.
+            Edit <Text style={styles.highlight}>src/app/navigation/pages/Home.tsx</Text> to change this screen
+            and then come back to see your edits.
           </Section>
           <Section title='See Your Changes'>
             <Text>
