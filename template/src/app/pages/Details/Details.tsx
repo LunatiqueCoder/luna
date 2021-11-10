9b230d39b7847c82a49af545e1bff66cd85700f2
Notorious112 <cristee95@gmail.com>
2021-11-10 14:38:06 +0200

    (feature) User color preferences reflected in example properly

--- template/src/app/pages/Details/Details.tsx
+++ template/src/app/pages/Details/Details.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {Text, StyleSheet, View} from 'react-native';
+import {Text, StyleSheet, View, Platform} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import {useColors} from 'app/hooks/useColors';
 
-export const Details = (): JSX.Element => (
-  <View style={styles.background}>
-    <Icon name='rocket' size={30} color='#900' />
-    <Text>If you see a rocket, everything is working!</Text>
-  </View>
-);
+export const Details = (): JSX.Element => {
+  const {backgroundColor} = useColors();
+
+  return (
+    <View style={[styles.background, Platform.OS === 'android' && {backgroundColor}]}>
+      <Icon name='rocket' size={30} color='#900' />
+      <Text>If you see a rocket, everything is working!</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   background: {
