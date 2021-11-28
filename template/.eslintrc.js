12eff72881b05a10ac9de6731f8858c989c1a178
Mike Hardy <github@mikehardy.net>
2021-11-28 15:16:18 -0500

    style(lint): get prettier and eslint to agree on switch/case spacing
    using spacing in eslint conflicts with spacing rules in prettier,
    and they *specifically* disagree on switch/case spacing in their
    default setups.
    
    Let Prettier control spacing, lint:all passes with fewer directives
    and pretty switch/case spacing.

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -30,7 +30,6 @@ module.exports = {
   },
   rules: {
     '@typescript-eslint/no-var-requires': 'off',
-    indent: ['error', 2],
     'linebreak-style': ['error', 'unix'], // https://adaptivepatchwork.com/2012/03/01/mind-the-end-of-your-line/
     quotes: ['error', 'single'],
     semi: ['error', 'always'],
