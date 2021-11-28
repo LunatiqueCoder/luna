898d4ffb976503bbcb0be084650f7d4af4323d4a
Mike Hardy <github@mikehardy.net>
2021-11-28 15:09:04 -0500

    style(lint): React import no longer needed in App-test
    verified working, allows removal of ts-ignore/eslint-ignore,
    which gets rid of a warning about an unreported eslint-ignore

--- template/src/__tests__/App-test.tsx
+++ template/src/__tests__/App-test.tsx
@@ -3,9 +3,6 @@
  */
 
 import 'react-native';
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore -- the import is needed for tests
-import React from 'react';
 import App from '../app/App';
 
 // Note: test renderer must be required after react-native.
