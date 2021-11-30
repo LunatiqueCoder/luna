ad04acb6c8eb77d6cdd2d8c88c66027dc3d3e1e9
Notorious112 <cristee95@gmail.com>
2021-11-30 10:52:06 +0200

    Feature - Add Web Vitals (create-react-app)

--- template/src/index.js
+++ template/src/index.js
@@ -5,6 +5,7 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import reportWebVitals from './reportWebVitals';
 
 // RNVI integration for web
 import './icons';
@@ -13,3 +14,8 @@ AppRegistry.registerComponent(appName, () => App);
 AppRegistry.runApplication(appName, {
   rootTag: document.getElementById('root'),
 });
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
