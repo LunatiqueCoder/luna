2a87a89f07dc5c8628133849944732f83ba686ed
Mike Hardy <github@mikehardy.net>
2022-03-05 15:35:34 -0500

    fix(ios): strip flow types for navigation stack
    Strange that this is necessary, but it avoids a javascript crash
    on iOS only, in combination with react-navigation
    
    See https://github.com/invertase/react-native-firebase-authentication-example/issues/9#issuecomment-1058810416

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   presets: [['module:metro-react-native-babel-preset', {useTransformReactJSXExperimental: true}]],
   plugins: [
+    ["@babel/plugin-transform-flow-strip-types"],
     [
       '@babel/plugin-transform-react-jsx',
       {
