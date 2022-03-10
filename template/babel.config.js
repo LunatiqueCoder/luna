481b6c9f3f6705047ba0b3e19249795a1169c7bf
Mike Hardy <github@mikehardy.net>
2022-03-09 20:02:08 -0500

    fix(lint): silence warnings during web production build
    craco / webpack was complaining about some of our language
    proposal choices not lining up correctly, this silences the warnings

--- template/babel.config.js
+++ template/babel.config.js
@@ -7,6 +7,10 @@ module.exports = {
       {
         runtime: 'automatic'
       }
-    ]
-  ]
+    ],
+    ['@babel/plugin-proposal-decorators', {legacy: true}],
+    ['@babel/plugin-proposal-class-properties', {loose: true}],
+    ["@babel/plugin-proposal-private-methods", { "loose": true }],
+    ["@babel/plugin-proposal-private-property-in-object", { "loose": true }]
+]
 };
