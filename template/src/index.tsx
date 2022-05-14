7a2c693ccf8b09f4ab1855d7dc036e32bc08e3b1
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2022-05-14 12:10:05 +0300

    createRoot - React 18

--- template/src/index.tsx
+++ template/src/index.tsx
@@ -2,18 +2,16 @@
  * When compiling for web, it will search for index.js on root/src.
  */
 
-import {AppRegistry} from 'react-native';
+import {createRoot} from 'react-dom/client';
 import App from './App';
-import {name as appName} from './app.json';
 import reportWebVitals from './reportWebVitals';
 
 // RNVI integration for web
 import './icons';
 
-AppRegistry.registerComponent(appName, () => App);
-AppRegistry.runApplication(appName, {
-  rootTag: document.getElementById('root'),
-});
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement!);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
