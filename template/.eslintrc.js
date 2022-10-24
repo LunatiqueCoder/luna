93867a5adf80ea5d870897afa961b83e349c2531
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2022-10-24 22:09:55 +0300

    0.70.3

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,43 +1,16 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-    jest: true,
-  },
-  extends: ['@react-native-community'],
+  root: true,
+  extends: '@react-native-community',
   parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   overrides: [
     {
-      files: ['*.ts?'],
+      files: ['*.ts', '*.tsx'],
       rules: {
+        '@typescript-eslint/no-shadow': ['error'],
+        'no-shadow': 'off',
         'no-undef': 'off',
       },
     },
   ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: ['react', '@typescript-eslint', 'prettier'],
-  settings: {
-    react: {
-      version: 'detect',
-    },
-  },
-  rules: {
-    '@typescript-eslint/no-var-requires': 'off',
-    'linebreak-style': ['error', 'unix'], // https://adaptivepatchwork.com/2012/03/01/mind-the-end-of-your-line/
-    semi: ['error', 'always'],
-    'prettier/prettier': 'error',
-    'react/display-name': 'off',
-    'react/no-unescaped-entities': 'off',
-    'react/jsx-uses-react': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'no-shadow': 'off',
-    '@typescript-eslint/no-shadow': ['error'],
-  },
 };
