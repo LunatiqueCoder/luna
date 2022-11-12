3537c7641c1ba1ca758b9dbc869d4f130dd99645
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2022-11-12 14:49:30 +0200

    remove comments

--- template/craco.config.ts
+++ template/craco.config.ts
@@ -77,32 +77,6 @@ const config: CracoConfig = {
 
       addBeforeLoader(webpackConfig, loaderByName('url-loader'), imageLoader);
 
-      // // https://github.com/facebook/create-react-app/pull/12021#issuecomment-1157617824
-      // const cjsLoader = {
-      //   test: /\.(cjs)$/,
-      //   // exclude: /@babel(?:\/|\\{1,2})runtime/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //     options: {
-      //       babelrc: false,
-      //       configFile: false,
-      //       compact: false,
-      //       presets: [
-      //         [
-      //           require.resolve('babel-preset-react-app/dependencies'),
-      //           {helpers: true},
-      //         ],
-      //       ],
-      //       cacheDirectory: true,
-      //       cacheCompression: false,
-      //       sourceMaps: true,
-      //       inputSourceMap: true,
-      //     },
-      //   },
-      // };
-      //
-      // addBeforeLoader(webpackConfig, loaderByName('babel-loader'), cjsLoader);
-
       return webpackConfig;
     },
   },
