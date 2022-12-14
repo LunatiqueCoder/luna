87af523d0a604093912aea9228841f8c6c476b41
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2022-12-14 02:25:56 +0200

    - babel.config.js - use next/babel preset when web usage is detected  - Remove @expo/next-adapter and @expo/config dependencies

--- template/babel.config.js
+++ template/babel.config.js
@@ -1,22 +1,30 @@
-module.exports = {
-  presets: [
-    [
-      'module:metro-react-native-babel-preset',
-      {useTransformReactJSXExperimental: true},
+// babel.config.js
+module.exports = function (api) {
+  // Detect web usage (this may change in the future if Next.js changes the loader)
+  const isWeb = api.caller(
+    caller =>
+      caller &&
+      (caller.name === 'babel-loader' ||
+        caller.name === 'next-babel-turbo-loader'),
+  );
+  return {
+    presets: [
+      // Only use next in the browser, it'll break your native project
+      isWeb && require('next/babel'),
+      [
+        'module:metro-react-native-babel-preset',
+        {useTransformReactJSXExperimental: true},
+      ],
+    ].filter(Boolean),
+    plugins: [
+      'react-native-reanimated/plugin',
+      ['@babel/plugin-transform-flow-strip-types'],
+      [
+        '@babel/plugin-transform-react-jsx',
+        {
+          runtime: 'automatic',
+        },
+      ],
     ],
-  ],
-  plugins: [
-    'react-native-reanimated/plugin',
-    ['@babel/plugin-transform-flow-strip-types'],
-    [
-      '@babel/plugin-transform-react-jsx',
-      {
-        runtime: 'automatic',
-      },
-    ],
-    ['@babel/plugin-proposal-decorators', {legacy: true}],
-    ['@babel/plugin-proposal-class-properties', {loose: true}],
-    ['@babel/plugin-proposal-private-methods', {loose: true}],
-    ['@babel/plugin-proposal-private-property-in-object', {loose: true}],
-  ],
+  };
 };
--- template/next.config.js
+++ template/next.config.js
@@ -1,16 +1,58 @@
-const {withExpo: withExpoConfig} = require('@expo/next-adapter');
 const withFonts = require('next-fonts');
 const withImages = require('next-images');
 const withPlugins = require('next-compose-plugins');
 const withTM = require('next-transpile-modules')(['solito']);
+const {Configuration, DefinePlugin} = require('webpack');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   webpack5: true,
+  webpack(config, options) {
+    // Mix in aliases
+    if (!config.resolve) {
+      config.resolve = {};
+    }
+
+    config.resolve.alias = {
+      ...(config.resolve.alias || {}),
+      // Alias direct react-native imports to react-native-web
+      'react-native$': 'react-native-web',
+      // Alias internal react-native modules to react-native-web
+      'react-native/Libraries/EventEmitter/RCTDeviceEventEmitter$':
+        'react-native-web/dist/vendor/react-native/NativeEventEmitter/RCTDeviceEventEmitter',
+      'react-native/Libraries/vendor/emitter/EventEmitter$':
+        'react-native-web/dist/vendor/react-native/emitter/EventEmitter',
+      'react-native/Libraries/EventEmitter/NativeEventEmitter$':
+        'react-native-web/dist/vendor/react-native/NativeEventEmitter',
+    };
+
+    config.resolve.extensions = [
+      '.web.js',
+      '.web.jsx',
+      '.web.ts',
+      '.web.tsx',
+      ...(config.resolve?.extensions ?? []),
+    ];
+
+    if (!config.plugins) {
+      config.plugins = [];
+    }
+
+    // Expose __DEV__ from Metro.
+    config.plugins.push(
+      new DefinePlugin({
+        __DEV__: JSON.stringify(process.env.NODE_ENV !== 'production'),
+      }),
+    );
+
+    // // Execute the user-defined webpack config.
+    // if (typeof nextConfig.webpack === 'function') {
+    //   return nextConfig.webpack(config, options);
+    // }
+
+    return config;
+  },
 };
 
-module.exports = withPlugins(
-  [withTM, withFonts, withImages, [withExpoConfig, {projectRoot: __dirname}]],
-  nextConfig,
-);
+module.exports = withPlugins([withTM, withFonts, withImages], nextConfig);
