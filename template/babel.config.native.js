28a8f535eea4e7ea5eae033279ddca92237c1d19
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-01-05 22:20:30 +0200

    metro babel withTamagui

--- template/babel.config.native.js
+++ template/babel.config.native.js
@@ -1,5 +1,34 @@
 // babel.config.js
+process.env.TAMAGUI_TARGET = 'native';
+
 module.exports = {
-  presets: [['module:metro-react-native-babel-preset']],
-  plugins: ['react-native-reanimated/plugin'],
+  presets: [
+    [
+      'module:metro-react-native-babel-preset',
+      {useTransformReactJSXExperimental: true},
+    ],
+  ],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        runtime: 'automatic',
+      },
+    ],
+    [
+      '@tamagui/babel-plugin',
+      {
+        components: ['tamagui'],
+        config: './tamagui',
+      },
+    ],
+    // be sure to set TAMAGUI_TARGET
+    [
+      'transform-inline-environment-variables',
+      {
+        include: 'TAMAGUI_TARGET',
+      },
+    ],
+  ],
 };
