9dc8079239525050c5b657fb54fc1d19ade9ed20
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-01-23 00:39:55 +0200

    bump packages and use platform specific animation drivers

---
+++ template/tamagui/animations.web.ts
@@ -0,0 +1,8 @@
+import {createAnimations} from '@tamagui/animations-css';
+
+export const animations = createAnimations({
+  fast: 'ease-in 150ms',
+  medium: 'ease-in 300ms',
+  slow: 'ease-in 450ms',
+  gelatine: 'elastic-spin 15s infinite ease',
+});
