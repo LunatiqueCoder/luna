4540cafb2959fae46e133db9005a70106b092073
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-01-24 21:38:10 +0200

    remove expo and fix animations.web.ts

--- template/android/app/src/main/java/com/projectname/MainApplication.java
+++ template/android/app/src/main/java/com/projectname/MainApplication.java
@@ -1,7 +1,4 @@
 package com.projectname;
-import android.content.res.Configuration;
-import expo.modules.ApplicationLifecycleDispatcher;
-import expo.modules.ReactNativeHostWrapper;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
@@ -16,7 +13,7 @@ import java.util.List;
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
-      new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
+      new DefaultReactNativeHost(this) {
         @Override
         public boolean getUseDeveloperSupport() {
           return BuildConfig.DEBUG;
@@ -45,7 +42,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected Boolean isHermesEnabled() {
           return BuildConfig.IS_HERMES_ENABLED;
         }
-      });
+      };
 
   @Override
   public ReactNativeHost getReactNativeHost() {
@@ -61,12 +58,5 @@ public class MainApplication extends Application implements ReactApplication {
       DefaultNewArchitectureEntryPoint.load();
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
-    ApplicationLifecycleDispatcher.onApplicationCreate(this);
-  }
-
-  @Override
-  public void onConfigurationChanged(Configuration newConfig) {
-    super.onConfigurationChanged(newConfig);
-    ApplicationLifecycleDispatcher.onConfigurationChanged(this, newConfig);
   }
 }
--- template/tamagui/animations.web.ts
+++ template/tamagui/animations.web.ts
@@ -1,7 +1,18 @@
-import {createAnimations} from '@tamagui/animations-css';
+import {createAnimations} from '@tamagui/animations-react-native';
 
 export const animations = createAnimations({
-  fast: 'ease-in 150ms',
-  medium: 'ease-in 300ms',
-  slow: 'ease-in 450ms',
+  fast: {
+    damping: 20,
+    mass: 1.2,
+    stiffness: 250,
+  },
+  medium: {
+    damping: 10,
+    mass: 0.9,
+    stiffness: 100,
+  },
+  slow: {
+    damping: 20,
+    stiffness: 60,
+  },
 });
