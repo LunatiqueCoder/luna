4390f417c39e456babf61b6ff1083b0fab3d2830
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-12 12:56:36 +0200

    refactor eslint config

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,17 +1,4 @@
 module.exports = {
   root: true,
   extends: ['@react-native-community', 'next/core-web-vitals'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
-  overrides: [
-    {
-      files: ['*.ts', '*.tsx'],
-      rules: {
-        '@typescript-eslint/no-shadow': ['error'],
-        'no-shadow': 'off',
-        'no-undef': 'off',
-        'react/react-in-jsx-scope': 'off',
-      },
-    },
-  ],
 };
