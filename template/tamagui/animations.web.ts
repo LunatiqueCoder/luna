632cb9409c24acb976453f52a5cc86e42f589335
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-14 00:35:09 +0200

    animations from @tamagui/config

--- template/tamagui/animations.web.ts
+++ template/tamagui/animations.web.ts
@@ -1,18 +1,9 @@
-import {createAnimations} from '@tamagui/animations-react-native';
+import {createAnimations} from '@tamagui/animations-css';
 
 export const animations = createAnimations({
-  fast: {
-    damping: 20,
-    mass: 1.2,
-    stiffness: 250,
-  },
-  medium: {
-    damping: 10,
-    mass: 0.9,
-    stiffness: 100,
-  },
-  slow: {
-    damping: 20,
-    stiffness: 60,
-  },
+  bouncy: 'ease-in 200ms',
+  lazy: 'ease-in 600ms',
+  slow: 'ease-in 500ms',
+  quick: 'ease-in 100ms',
+  tooltip: 'ease-in 400ms',
 });
