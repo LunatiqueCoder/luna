f976932a57d5d38365f8582c23c27f1c0e4689f0
Ovidiu Cristescu <55203625+criszz77@users.noreply.github.com>
2023-02-15 00:19:13 +0200

    ☝️Bump React Native and Tamagui + new themes

--- template/tamagui/tamagui.config.ts
+++ template/tamagui/tamagui.config.ts
@@ -5,6 +5,10 @@ import {media, mediaQueryDefaultActive} from './mediaQueries';
 import {animations} from './animations';
 import fonts from './fonts';
 
+/* Alter the themes however you wish */
+themes.light_Button.background = 'hsl(0, 0%, 92%)';
+themes.light.background = 'rgb(242, 242, 242)'; // @react-navigation - DefaultTheme.colors.background
+
 const appConfig = createTamagui({
   shouldAddPrefersColorThemes: true,
   themeClassNameOnRoot: true,
