b0b7c37389957b918531c5d58d01290a48f57d1c
Ovidiu Cristescu <55203625+LunatiqueCoder@users.noreply.github.com>
2023-04-09 02:17:02 +0300

    remove tests for vanilla

--- templates/vanilla/template/src/__tests__/App-test.tsx
+++ templates/vanilla/template/src/__tests__/App-test.tsx
@@ -2,17 +2,17 @@
  * @format
  */
 
-import 'react-native';
-import App from '../App';
-
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
 // FIXME need reanimated update, see https://github.com/software-mansion/react-native-reanimated/issues/3355
 if (process.browser) {
   // @ts-ignore
   window._frameTimestamp = null;
 }
+
+import 'react-native';
+import App from '../App';
+
+// Note: test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   renderer.create(<App />);
