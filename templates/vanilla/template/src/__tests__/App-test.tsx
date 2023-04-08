0168102c13947618f9431d66074f3fdcc7077d08
Ovidiu Cristescu <55203625+LunatiqueCoder@users.noreply.github.com>
2023-04-09 01:50:45 +0300

    improve pipeline (9) - reanimated fix

--- templates/vanilla/template/src/__tests__/App-test.tsx
+++ templates/vanilla/template/src/__tests__/App-test.tsx
@@ -8,6 +8,12 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+// FIXME need reanimated update, see https://github.com/software-mansion/react-native-reanimated/issues/3355
+if (process.browser) {
+  // @ts-ignore
+  window._frameTimestamp = null;
+}
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
