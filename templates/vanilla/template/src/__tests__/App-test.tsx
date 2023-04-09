e990f4654992aaa827b613ccc55188f0e80eef53
Ovidiu Cristescu <55203625+LunatiqueCoder@users.noreply.github.com>
2023-04-09 03:53:56 +0300

    fix tests (4)

--- templates/vanilla/template/src/__tests__/App-test.tsx
+++ templates/vanilla/template/src/__tests__/App-test.tsx
@@ -8,6 +8,10 @@ import App from '../App';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
+jest.mock('react-native-reanimated', () =>
+  require('react-native-reanimated/mock'),
+);
+
 it('renders correctly', () => {
   renderer.create(<App />);
 });
